df24f2c5af1f70082010a8bee05962648dea616a
Chandler Qian <chandler20060524@gmail.com>
2026-09-10 17:10:39 -0600

    Created a new Android project ListyCity.

---
+++ app/src/main/java/com/demingqian/listycity/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.demingqian.listycity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.demingqian.listycity.ui.theme.ListyCityTheme
+import androidx.compose.runtime.mutableStateListOf
+import kotlin.coroutines.EmptyCoroutineContext.get
+
+class CityRepository {
+    private val _cities = mutableStateListOf(
+        "Edmonton", "Vancouver", "Moscow", "Sydney", "Ber;in", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"
+    )
+
+    fun cities(): List<String> {
+        get() = _cities
+    }
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ListyCityTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ListyCityTheme {
+        Greeting("Android")
+    }
+}
